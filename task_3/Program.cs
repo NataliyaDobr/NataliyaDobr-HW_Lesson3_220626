b8ac58c5ad56b7a02ff7c44eeca165674f8f882c
nataliya <n.m.suvorova@mail.ru>
2022-06-27 23:55:24 +0300

    task2

---
+++ task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27. 
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+int N = Prompt("Введите число N ");
+int i = 1;
+int temp1;
+
+while (i<=N) {
+    temp1=i*i*i;
+    System.Console.WriteLine(temp1);
+    i++;
+}
